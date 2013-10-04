e0299cf82a55c6722c3c34d48cefc4bef48d2f4e
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2013-10-05 00:37:20 +0200

    jQuery plugin repository files preparation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,22 @@
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt){
 
     grunt.initConfig({
-        banner: grunt.file.read("header.js"),
         uglify: {
-            dist: {
-                options: {
-                    banner: "<%= banner %>",
-                    sourceMap: function (path) {
-                        return path + ".map";
-                    }
-                },
-                files: {
-                    'jquery-bend-gauge.min.js': [ 'jquery-bend-gauge.js' ]
-                }
+          dist: {
+            options: {
+              sourceMap: function(path) {
+                return path + ".map";
+              },
+            },
+            files: {
+              'jquery.bend-gauge.min.js': [ 'jquery.bend-gauge.js' ]
             }
+          }
         }
     });
-
+    
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('default', ['uglify']);
 
