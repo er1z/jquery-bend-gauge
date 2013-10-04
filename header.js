f02858a022c024836612b5b367cad3783d9dfc75
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2013-10-05 00:49:47 +0200

    license texts polishing

--- header.js
+++ header.js
@@ -2,6 +2,8 @@
  * jQuery Bend Gauge
  * A simple circular value indicator written in RaphaelJS.
  *
+ * Released under terms of MIT License.
+ *
  * Created by eRIZ - http://przemyslaw.pawliczuk.pl
  * Brought by h2p.pl
  */
