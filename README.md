7106c9a1a7017d29c87ffb87491cbee36c7754b0
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2013-10-05 12:28:58 +0200

    initialized pages branch

--- README.md
+++
@@ -1,4 +0,0 @@
-jquery-bend-gauge
-=================
-
-Circular value indicator written in jQuery and RaphaelJS
--- header.js
+++
@@ -1,9 +0,0 @@
-/**
- * jQuery Bend Gauge
- * A simple circular value indicator written in RaphaelJS.
- *
- * Released under terms of MIT License.
- *
- * Created by eRIZ - http://przemyslaw.pawliczuk.pl
- * Brought by h2p.pl
- */
